f92d4b002a41d7a36a465f256a05c1bf9c0028fe
Sergey <sergey-kashurin@mail.ru>
2019-06-03 19:42:06 +0300

    Added user registration /register/

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,5 +1,26 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
-from .models import ShopUser
+from authapp.models import ShopUser
+
+
+
+class ShopUserRegisterForm(UserCreationForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'password1', 'password2', 'email', 'age', 'avatar')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+    def clean_age(self):
+        data = self.cleaned_data['age']
+        if data < 18:
+            raise forms.ValidationsError('Вы слишком молоды!')
+
+        return data
 
 
 class ShopUserLoginForm(AuthenticationForm):
