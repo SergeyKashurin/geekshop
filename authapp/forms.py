2bc207b32ff61ea15ecd21df9484f0512a5be415
Sergey <sergey-kashurin@mail.ru>
2019-06-04 09:33:38 +0300

    Added editing for logined users. Add styles for new forms.

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,8 +1,28 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.forms import AuthenticationForm
 from authapp.models import ShopUser
 
 
+class ShopUserEditForm(UserChangeForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'email', 'age', 'avatar', 'password')
+
+        def __init___(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            for field_name, field in self.fields.items():
+                field.widget.attrs['class'] = 'form-control'
+                field.help_text = ''
+                if field_name == 'password':
+                    field.widget = forms.HiddenInput()
+
+    def clean_age(self):
+        data = self.cleaned_data['age']
+        if data < 18:
+            raise forms.ValidationError('Вы слишком молоды!')
+
+        return data
 
 class ShopUserRegisterForm(UserCreationForm):
     class Meta:
@@ -18,7 +38,7 @@ class ShopUserRegisterForm(UserCreationForm):
     def clean_age(self):
         data = self.cleaned_data['age']
         if data < 18:
-            raise forms.ValidationsError('Вы слишком молоды!')
+            raise forms.ValidationError('Вы слишком молоды!')
 
         return data
 
